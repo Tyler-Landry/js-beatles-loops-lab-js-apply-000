59926707daef82bbbfb1caf22d3d98dea32eb50a
Tyler Landry <tyler.landry1996@outlook.com>
2019-03-07 00:45:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-function theBeatlesPlay() {
-  const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-  const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-
+function theBeatlesPlay(musicians, instruments) {
+const Array = {}
 }
 
